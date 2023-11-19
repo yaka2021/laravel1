2990d0829cd6831a6b40ead8029ec6a219077a4d
yaka2021 <cbky1211051@gn.iwasaki.ac.jp>
2023-11-19 15:03:35 +0900

    st4

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Practice;
 
 class PracticeController extends Controller
 {
@@ -18,5 +19,10 @@ class PracticeController extends Controller
     {
         $test = 'test';
             return view('practice3', ['testParam' => $test]);
+    }
+    public function getPractice()
+    {
+        $practice = Practice::all();
+        return response()->json($practice);
     }
 }
